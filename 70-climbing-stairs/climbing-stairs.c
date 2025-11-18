ce80d7cee67d8c62678be967f1ec12765b2c6bc1
Lara Askovic <160878434+laraaskovic@users.noreply.github.com>
2025-11-17 20:01:51 -0500

    Time: 0 ms (100.00%) | Memory: 8 MB (15.28%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.c
@@ -0,0 +1,20 @@
+// */int climbStairs(int n) {
+//     if(n==1) return 1;
+//     if(n==2) return 2;
+    
+
+//     return climbStairs(n-1) + climbStairs(n-2);  
+
+// }*/
+
+int climbStairs(int n) {
+    if (n <= 2) return n;
+
+    int a = 1, b = 2;
+    for (int i = 3; i <= n; i++) {
+        int c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
